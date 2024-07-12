a1f09d21fdb53c61f241622ff735ce4d8aff0beb
adayilmax <144628453+adayilmax@users.noreply.github.com>
2024-07-12 17:37:11 +0300

    first commit

---
+++ test.js
@@ -0,0 +1,43 @@
+var test = 1
+// console.log(test)
+
+// test = 105
+// console.log(test)
+
+// test = "ada"
+// console.log(test)
+
+// test = true
+// console.log(test)
+
+// test = undefined // same as declaring
+// console.log(test)
+
+// test = null
+// console.log(test)
+
+// console.log(typeof test)
+
+var a = "test"
+// console.log(a)
+
+var b = "start"
+
+// console.log(a+b)
+// console.log(a+" "+b)
+
+var c = `${a}${b}`
+// console.log(c)
+
+// c = `${a} ${b} al ${3+5}`
+// console.log(c)
+
+console.log(a[0])
+
+console.log(a.charAt(0))
+
+console.log(b.indexOf("t"))
+
+console.log(b.indexOf("t",3))
+
+console.log(b.lastIndexOf("t"))
